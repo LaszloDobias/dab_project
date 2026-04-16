6a19f164800a226bf3b5f5bfca8d9f78e87d5099
Laszlo Dobaisz <laszlo.dobiasz@wpp.com>
2026-04-17 09:46:11 +1000

    k

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ setup(
     name="dab_project",
     version="0.0.1",
     description="This contains the code in the ./src directory of the project",
-    author="Your Name",
+    author="Laszlo Dobiasz",
     packages=find_packages(where="./src"),
     package_dir={"": "./src"},
     install_requires=["setuptools"],
+    entry_points={"packages": ["main = wheel_upload.entry:dummy"]},
 )
